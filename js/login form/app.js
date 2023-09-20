2bf074ee82859e9f79f92dc37f8e2ad941f3cf25
minuup <dn1088@kakao.com>
2023-09-20 21:20:55 +0900

    prevent

--- js/login form/app.js
+++ js/login form/app.js
@@ -2,12 +2,22 @@ const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const loginButton = document.querySelector("#login-form button");
 
+const link =document.querySelector("a")
+
 function onLoginSubmit(event) {
     evnet.preventDefault();
     console.log(loginInput.value);
     }
 
+    function handleLinkClick(event){
+        event.preventDefault()
+        console.dir(event);
+        alert("clicked");
+
+    }
+
 loginForm.addEventListener("submit",onLoginSubmit);
+link.addEventListener("click",handleLinkClick);
 
 
 
